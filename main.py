e0512536e35a2c3e24e74507a056c39bca3fadea
Ale Sanchez <alesanmed@alum.us.es>
2016-07-18 18:27:09 +0200

    Uso de User-Agent aleatorio, tiempo de espera entre peticiones y paso de precio a la función de interpretación de viaje

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen
+import urllib.request
+from tools.parse_travel import parse_travel
+from tools.user_agent import get_user_agent
+import time
 
 URL = "http://www.exprimeviajes.com/"
 
-# document = urlopen(URL)
+"""
+req = urllib.request.Request(
+                    URL,
+                    data=None,
+                    headers= {
+                        'User-Agent': get_user_agent()
+                        }
+                    )
+document = urllib.request.urlopen(req)
+"""
 
 # Only for the development stage
 with open('test.txt', 'r') as file:
@@ -17,6 +29,8 @@ sales = []
 for h2 in web.findAll('h2', class_="entry-title"):
     link = h2.a['href']
     title = h2.a.text.strip().split()
-    sales.append((link, int(title[title.index("EUROS") - 1])))
+    price = int(title[title.index("EUROS") - 1])
+    sales.append(parse_travel(link, price))
+    time.sleep(3)
 
 print(sales)
