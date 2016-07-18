3192a4c979d41ac6ea04b8c81c0d171cb4e07cb8
Ale Sanchez <alesanmed@alum.us.es>
2016-07-18 18:26:35 +0200

    Importado genérico del archivo de credenciales

--- maps/coordinates.py
+++ maps/coordinates.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import googlemaps
 import os, sys
-sys.path.append(os.path.join(
-                os.path.dirname(
+if os.path.relpath(".", "..") != "viajes":
+    sys.path.append(os.path.join(
                     os.path.dirname(
-                        os.path.abspath(__file__))),
-                "credentials"))
-
-from maps import api_key
+                        os.path.dirname(
+                            os.path.abspath(__file__))),
+                    "credentials"))
+    from maps import api_key
+else:
+    from credentials.maps import api_key
 
 def get_coordinates(location):
     gmaps = googlemaps.Client(key=api_key)
@@ -17,5 +19,5 @@ def get_coordinates(location):
     return "{0},{1}".format(geocode_res['geometry']['location']['lat'],
                             geocode_res['geometry']['location']['lng'])
                             
-if __name__ == "__main__":
-    print(get_coordinates("Sevilla"))
+# if __name__ == "__main__":
+    # print(get_coordinates("Sevilla"))
