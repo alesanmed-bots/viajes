6daff74c1b80f2e363895c02b3a2f2aff0a050d2
Ale Sanchez <alesanmed@alum.us.es>
2016-07-20 21:31:07 +0200

    Cambiadas las transacciones para trabajar con mongoengine

--- mongodb/transactions.py
+++ mongodb/transactions.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
-
-from pymongo import MongoClient
+from mongoengine import connect as mongo_connect
+from mongodb.documents.Travel import Travel
 
 def connect():
-    client = MongoClient()
-    db = client.travel_bot
-    
-    return client, db
-    
-def disconnect(client):
-    client.close()
+    mongo_connect('travel_bot')
     
 def get_last_to_check():
-    client, db = connect()
+    last_to_check = Travel.objects(last=True).first()
     
-    last_to_check = db.last_travel.find({}, limit=1)[0]
+    last_url = None
+
+    if last_to_check is not None:    
+        last_url = last_to_check.url
+        
+    return last_url
     
-    disconnect(client)    
+def update_last_to_check(travel_url):
+    Travel.objects(last=True).update_one(set__last=False)
     
-    return last_to_check
+    Travel.objects(url=travel_url).update_one(set__last=True)
     
-def update_last_to_check(travel):
-    client, db = connect()
+def save_travel(travel):
+    print(travel)
+    to_save = Travel(**travel)
     
-    db.last_travel.remove({})
-    
-    db.last_travel.insert_one(travel)
-    
+    to_save.save()
