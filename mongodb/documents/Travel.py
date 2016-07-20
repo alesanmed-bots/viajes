9a544b459f04c8bfe9826a96d12d6bf261ed5b7f
Ale Sanchez <alesanmed@alum.us.es>
2016-07-20 21:31:17 +0200

    Objeto representante de la entidad Viajes

--- mongodb/documents/Travel.py
+++ mongodb/documents/Travel.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 from mongoengine import Document
-from mongoengine.fiedls import FloatField
-from mongoengine.fiedls import StringField
+from mongoengine.fields import FloatField, StringField, URLField, BooleanField
 import re
 
 
 class Travel(Document):
     price = FloatField(required=True, min_value=0.0)
     date = StringField(required=True,
-                       regex=re.compile("^(0[1-9]|[12][0-9]|3[01])-\
-                       (0[1-9]|1[012])-(19|20)\d\d$"))
+                       regex=re.compile("^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[012])-(19|20)\d\d$"))
+    departure = StringField(required=True)
+    destination = StringField(required=True)
+    return_to = StringField(required=True)
+    url = URLField(required=True, unique=True)
+    ticket_type = StringField(required=True)
+    last = BooleanField(required=True, default=False)
